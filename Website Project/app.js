c7b4d6b14541aaa558e351e0ef856b6c6cb376d3
BHillard1717 <hillarbp@mail.uc.edu>
2022-02-17 16:53:03 -0500

    Built Navbar;Added Links;Added resume - v00.00.02

--- Website Project/app.js
+++ Website Project/app.js
@@ -1,13 +1,11 @@
 'use strict'
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click',function() {
-    document.body.classList.toggle('dark-theme');
 
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark"
-    }else{
-        this.textContent = "Light"
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
-    console.log('curr class name : ' + className);
-});
+  }
