eb7d6d483de2f9442d078e8d8401bf3676f378bb
BHillard1717 <hillarbp@mail.uc.edu>
2022-03-07 18:11:58 -0500

    Modified Screen Wipe Effects - v00.00.06

--- Website Project/app.js
+++ Website Project/app.js
@@ -1,5 +1,10 @@
 'use strict'
-
+var flag = true;
+const mainDivOptions = [];
+mainDivOptions[0] = '<img src="sources/chilled-cow-lofi-gif.gif" alt="TempImg" class="mainVideo"><h2>Goals</h2><p id="msg" onclick="homeScreenFade()">To Do:</p><ul><li id="YouTubeLink"><a href="https://www.youtube.com/watch?v=ChgK3V0Eezo" target="_blank">Among Us</a></li></ul><p id="shufflePara" style="width: 100px">This is an example of a multiline paragraph. I want to test how the shuffle text will wokr for multiple linesof text that is broken up into multiple lines When the amount of lines changes I wonder what it will look like.</p>';
+mainDivOptions[1] = '<img src="sources/chilled-cow-lofi-gif.gif" alt="TempImg" class="mainVideo">';
+mainDivOptions[4] = '<ul><li><a href="https://github.com/BHillard1717">Check out my Github!</a></li><li><a href="https://www.linkedin.com/in/brandon-hillard/">Add me on LinkedIn</a></li><li><a href="mailto: bhillard1717@gmail.com">Contact via Email!</a></li><li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">My solution to the traveling salesman problem in O(log(n)) time!</a></li></ul>';
+mainDivOptions[5] = '<embed src="sources/Resume - Brandon Hillard.pdf" class="pdfView" />';
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -24,26 +29,54 @@ function homeScreenFade(){
   });
 }
 
-function pageTransition(){
+function pageTransition(clickedTab){
+  var currentTab = document.getElementsByClassName("current");
+    console.log(currentTab);
+  //currentTab[0].className = "basic";
   console.log("PING");
-  //get the location of the line
-  $( ".transition").toggleClass( "anim-trans" );
+  console.log(clickedTab);
+  if(clickedTab.className == "current"){
+    //do nothing
+  }else{
+    //change the current tab
+    currentTab[0].className = "basic";
+    clickedTab.className = "current";
+    $( ".transition").toggleClass( "anim-trans-start" );
+    //wait till its over
+    //change the content
+      //content will be stored as div objects and the main div will be set equal
+      //to an array index based on the tab clicked 
+    //then raise the thing
+    setTimeout(() => {
+      if(clickedTab.id === "linksHeading"){
+        document.getElementById("mainDiv").innerHTML = mainDivOptions[4];
+      }else if(clickedTab.id === "resumeHeading"){
+        document.getElementById("mainDiv").innerHTML = mainDivOptions[5];
+      }else if(flag){
+      document.getElementById("mainDiv").innerHTML = mainDivOptions[1];
+      }else{
+        document.getElementById("mainDiv").innerHTML = mainDivOptions[0];
+      }
+      flag = !flag;
 
-  //increase the size over time to cover the entire bottom of the screen
-  
-  //await delay(1000);
-  //goalTab.className = "topNavBackground";
-
-  //check the status of the navbar //might not be needed
-  //if(navBarStatus.className == "topnav"){
-    
-  //}else{
-    //in responsive mode so just hard transition or wipe from below the bar?
-  //}
-  //increase it to the width of the page
-  //have a block grow in size over the page
-  //change the contents under the block
-  //block shrinks back
-  //line shrinks back to 0
+    }, 1000);
+    setTimeout(() => {
+      console.log("test");
+      $( ".transition").toggleClass( "anim-trans-start" );
+      $( ".transition").toggleClass( "anim-trans-end" );
+      
+    }, 1100);
+    setTimeout(() => {
+      console.log("test");
+      $( ".transition").toggleClass( "anim-trans-end" );
+      //$( ".transition").toggleClass( "anim-trans-start" );
+      
+    }, 2200);
+    //$( "anim-trans-start").toggleClass( ".transition" );
+    //set class to current
+  }
+  //$( ".transition").toggleClass( "anim-trans-start" );
+  //$( ".anim-trans-start").toggleClass( "anim-trans-end" );
+  //$( ".anim-trans-end").toggleClass( "transition" );
 }
 
